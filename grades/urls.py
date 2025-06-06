d8a12683ace8d46f82362464866c2644a73813c5
alexis21212121 <juvertalexisdulay@gmail.com>
2025-06-06 20:26:48 +0800

    adding static files configuration

--- grades/urls.py
+++ grades/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GradeViewSet
 
+# this is our root url file
+
+
 router = DefaultRouter()
 router.register(r'grades', GradeViewSet, basename='grade')
 
